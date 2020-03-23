06cff783ea56de4a389c06a53e6fd2c7639247a8
AlexandreRicart <58735486+AlexandreRicart@users.noreply.github.com>
2020-03-23 15:41:03 +0100

    Ajout l'exercice recherche internet !
    :D

---
+++ RecherchesInternet/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ArraySize;
+            int[] MyArray;
+            int i;
+            int Read;
+
+            //Proposer à l'utilisateur de faire un tableau :
+            Console.WriteLine("Bonjour, vous voulez un tableau de quelle taille ?\n");
+            //Transformer le tout en entier
+            ArraySize = Console.ReadLine();
+            Read = Int32.Parse(ArraySize);
+            //Le tableau va prendre la taille X
+            MyArray = new int[Read];
+            Console.WriteLine("Voila un tableau de "+ArraySize+" cases : \n");
+            for (i = 0; i < Read; i++) {
+                //Afficher le tableau
+                Console.WriteLine(i+1 );
+            }
+        }
+    }
+}
---
+++ RecherchesInternet/Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+
+            //Proposer à l'utilisateur de faire un tableau :
+            Console.WriteLine("Bonjour, a qui ai-je l'honneur ?\n");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Bonjour "+Name);
+
+        }
+    }
+}
